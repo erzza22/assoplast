a9e7e840dab0fbc730e9db1138278f88ded9af38
exahzr <erzahaziri25@gmail.com>
2020-01-31 14:46:56 +0100

    Add files via upload

---
+++ Repositories/GenericRepository.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC.Entities;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MVC.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly AssoplastPlannerContext _context;
+
+        public GenericRepository(AssoplastPlannerContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task Add(T entity)
+        {
+            await _context.AddAsync(entity);
+        }
+
+        public async Task AddRange(IEnumerable<T> entities)
+        {
+            await _context.AddRangeAsync(entities);
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicte, params Expression<Func<T, object>>[] includes)
+        {
+            var query = _context.Set<T>().Where(predicte);
+            return includes.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+        }
+
+        public async Task<T> FindOne(Expression<Func<T, bool>> predicte)
+        {
+            return await _context.Set<T>().FirstOrDefaultAsync(predicte);
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetById(string id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+        public async Task<T> GetById(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public IQueryable<T> Query()
+        {
+            return _context.Set<T>().AsQueryable();
+        }
+
+        public void Remove(T entity)
+        {
+            _context.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _context.RemoveRange(entities);
+        }
+
+        public async Task<bool> SaveChanges()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Update(entity);
+        }
+
+
+    }
+}
