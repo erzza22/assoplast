629e9c298c866007bda311455debc3b4a8b5e0e3
exahzr <erzahaziri25@gmail.com>
2020-01-31 14:42:44 +0100

    Add files via upload

---
+++ Enums/DestinationTypeEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.Enums
+{
+    public enum DestinationTypeEnum
+    {
+        Recupero = 1,
+        Smaltimento = 2
+    }
+}
