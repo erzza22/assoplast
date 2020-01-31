a3d304cfd9abe7bc808fd1612a5c57cf2bf83420
exahzr <erzahaziri25@gmail.com>
2020-01-31 14:38:48 +0100

    Add files via upload

---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MVC.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        
+
+    }
+}
